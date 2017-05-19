818ea2a8a0b5de1e61ce7667bf8b59d216bd3cec
Bojie Jiang <jiangbojie@gmail.com>
2017-05-18 23:32:57 -0700

    Hello world

---
+++ src/us/bojie/trykotlin/Main.kt
@@ -0,0 +1,13 @@
+package us.bojie.trykotlin
+
+/**
+ * Created by bojiejiang on 5/18/17.
+ */
+
+
+fun main(args: Array<String>) {
+    println("Hello world")
+    println(Main(0, "Bojie"))
+}
+
+data class Main(val id :Int, val name: String)
