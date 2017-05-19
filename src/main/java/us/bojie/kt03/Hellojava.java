24fc744b0d56e016ed46b380e75190288c055bc1
Bojie Jiang <jiangbojie@gmail.com>
2017-05-19 00:08:42 -0700

    Setup gradle env and two intro files

---
+++ src/main/java/us/bojie/kt03/Hellojava.java
@@ -0,0 +1,12 @@
+package us.bojie.kt03;
+
+/**
+ * Created by bojiejiang on 5/19/17.
+ */
+public class Hellojava {
+
+    public static void main(String[] args) {
+        User user = new User(0, null);
+        System.out.println(user);
+    }
+}
---
+++ src/main/kotlin/us/bojie/kt03/User.kt
@@ -0,0 +1,7 @@
+package us.bojie.kt03
+
+/**
+ * Created by bojiejiang on 5/19/17.
+ */
+
+data class User(val id: Int, val name: String?)
